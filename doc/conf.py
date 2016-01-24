cd1da68dcc6b4be40401177322b94ed228ffab75
ethereal <ethereal@ethv.net>
2016-01-23 21:41:51 -0800

    Updated copyright year.

--- doc/conf.py
+++ doc/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'kriti'
-copyright = '2015, ethereal'
+copyright = '2012-2016, ethereal'
 author = 'ethereal'
 
 # The version info for the project you're documenting, acts as replacement for
